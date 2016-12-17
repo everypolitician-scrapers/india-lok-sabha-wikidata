d40cfb8f2cd8768fdcb539278321970bcce7f6e5
Tony Bowden <tony@tmtm.com>
2016-12-17 09:46:31 +0000

    Remove batch processing

--- scraper.rb
+++ scraper.rb
@@ -6,8 +6,8 @@ require 'wikidata/fetcher'
 names = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_16th_Lok_Sabha',
   xpath: '//table[.//th[.="Constituency"]]//tr/td[2]//a[not(@class="new")]/@title',
-) 
+)
 
 by_category = WikiData::Category.new( 'Category:16th Lok Sabha members', 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names | by_category }, batch_size: 100)
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names | by_category })
