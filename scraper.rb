84d64d2fabb5bd7ee7a3c8dbab1a566aae4883fa
Tony Bowden <tony@tmtm.com>
2018-08-13 13:11:08 +0100

    Find text in header
    Find this even where there's a sort-arrow on the column.

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,7 @@ require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_16th_Lok_Sabha',
-  xpath: '//table[.//th[.="Constituency"]]//tr/td[2]//a[not(@class="new")]/@title',
+  xpath: '//table[.//th[contains(.,"Constituency")]]//tr/td[2]//a[not(@class="new")]/@title',
 )
 
 by_category = WikiData::Category.new( 'Category:16th Lok Sabha members', 'en').member_titles
