be5a18bde2894199ae12743d01f7dfb58ac52634
Tony Bowden <tony@tmtm.com>
2018-08-16 07:54:00 +0100

    Only fetch via P39s
    The P39 data is believed to be complete, so we can use that exclusively
    now. This should also make it simpler to reconcile, as we won't be
    dealing with anyone fetched in error by other means.

--- scraper.rb
+++ scraper.rb
@@ -3,14 +3,7 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
-  url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_16th_Lok_Sabha',
-  xpath: '//table[.//th[contains(.,"Constituency")]]//tr/td[2]//a[not(@class="new")]/@title',
-)
-
-by_category = WikiData::Category.new( 'Category:16th Lok Sabha members', 'en').member_titles
-
 sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39 wd:Q16556694 ; pq:P2937 wd:Q15978395 ] }'
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names | by_category })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids)
